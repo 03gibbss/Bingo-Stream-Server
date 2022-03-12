bf8492cc6d7040253832744447d68df6db392b04
03gibbss <sam-gibbs1@hotmail.co.uk>
2022-03-12 18:00:54 +0000

    Code tidy and added functionality for second and third OBS instances

--- libs/OBS.js
+++ libs/OBS.js
@@ -1,4 +1,4 @@
-const OBSWebSocket = require('obs-websocket-js');
+const OBSWebSocket = require("obs-websocket-js");
 
 module.exports = class OBS {
   constructor(system, io, config) {
@@ -12,104 +12,131 @@ module.exports = class OBS {
 
   connect() {
     return new Promise((resolve, reject) => {
-      this.obs.connect({
-        address: `${this.config.address !== '' && this.config.address !== undefined ? this.config.address : '127.0.0.1'}:${this.config.port !== '' && this.config.port !== undefined ? this.config.port : '4444'}`,
-        password: this.config.password
-      })
+      this.obs
+        .connect({
+          address: `${
+            this.config.address !== "" && this.config.address !== undefined
+              ? this.config.address
+              : "127.0.0.1"
+          }:${
+            this.config.port !== "" && this.config.port !== undefined
+              ? this.config.port
+              : "4444"
+          }`,
+          password: this.config.password,
+        })
         .then(() => {
           console.log(`${this.config.name} connected`);
           resolve();
         })
-        .catch(err => {
+        .catch((err) => {
           reject(err);
-        })
-    })
+        });
+    });
   }
 
   deleteSceneItem(scene, itemId) {
     return new Promise((resolve, reject) => {
-      this.obs.send('DeleteSceneItem', {
-        'scene': scene,
-        'item': {
-          'id': itemId
-        }
-      })
+      this.obs
+        .send("DeleteSceneItem", {
+          scene: scene,
+          item: {
+            id: itemId,
+          },
+        })
         .then(() => {
           console.log(`${itemId} deleted`);
-          resolve()
+          resolve();
         })
-        .catch(err => reject(err));
-    })
+        .catch((err) => reject(err));
+    });
   }
 
   setSceneItemProperties(scene, itemId, xposition, yposition, scale) {
     return new Promise((resolve, reject) => {
-      this.obs.send('SetSceneItemProperties', {
-        'scene-name': scene,
-        'item': {
-          'id': itemId
-        },
-        'position': {
-          'x': xposition,
-          'y': yposition
-        },
-        'scale': {
-          'x': scale,
-          'y': scale
-        }
-      })
+      this.obs
+        .send("SetSceneItemProperties", {
+          "scene-name": scene,
+          item: {
+            id: itemId,
+          },
+          position: {
+            x: xposition,
+            y: yposition,
+          },
+          scale: {
+            x: scale,
+            y: scale,
+          },
+        })
         .then(() => {
           console.log(`${itemId} Properties Set`);
           resolve();
         })
-        .catch(err => reject(err));
-    })
+        .catch((err) => reject(err));
+    });
   }
 
   addSceneItem(scene, sourceName) {
     return new Promise((resolve, reject) => {
-      this.obs.send('AddSceneItem', {
-        'sceneName': scene,
-        'sourceName': sourceName
-      })
-        .then(data => {
+      this.obs
+        .send("AddSceneItem", {
+          sceneName: scene,
+          sourceName: sourceName,
+        })
+        .then((data) => {
           console.log(`${data.itemId} added`);
           resolve(data.itemId);
         })
-        .catch(err => reject(err));
-    })
+        .catch((err) => reject(err));
+    });
+  }
+
+  setCurrentScene(scene) {
+    return new Promise((resolve, reject) => {
+      this.obs
+        .send("SetCurrentScene", {
+          "scene-name": scene,
+        })
+        .then(() => {
+          resolve();
+        })
+        .catch((err) => reject(err));
+    });
   }
 
   getSceneItemList(scene) {
     return new Promise((resolve, reject) => {
-      this.obs.send('GetSceneItemList', {
-        'sceneName': scene
-      })
-        .then(data => {
-          resolve(data.sceneItems)
+      this.obs
+        .send("GetSceneItemList", {
+          sceneName: scene,
         })
-        .catch(err => reject(err));
-    })
+        .then((data) => {
+          resolve(data.sceneItems);
+        })
+        .catch((err) => reject(err));
+    });
   }
 
   getSceneItemProperties(scene, itemId) {
     return new Promise((resolve, reject) => {
-      this.obs.send('GetSceneItemProperties', {
-        'scene-name': scene,
-        'item': {
-          'id': itemId
-        }
-      })
-        .then(data => {
-          resolve(data)
+      this.obs
+        .send("GetSceneItemProperties", {
+          "scene-name": scene,
+          item: {
+            id: itemId,
+          },
         })
-        .catch(err => reject(err));
-    })
+        .then((data) => {
+          resolve(data);
+        })
+        .catch((err) => reject(err));
+    });
   }
 
   events() {
-    this.obs.on('error', err => {
+    this.obs.on("error", (err) => {
       console.log(err);
-    })
+    });
   }
-}
+};
--- server.js
+++ server.js
@@ -1,329 +1,418 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const colors = require('colors');
-const EventEmitter = require('events');
+const colors = require("colors");
+const EventEmitter = require("events");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
-const DB = require('./libs/DB');
-
-const OBS = require('./libs/OBS');
+const OBS = require("./libs/OBS");
 
 const system = new EventEmitter();
 
+const useOBS2 = false;
 const useOBS3 = false;
 
 let state = {
   OBS1: {
-    connected: false
+    connected: false,
   },
   OBS2: {
-    connected: false
+    connected: false,
   },
   OBS3: {
-    connected: false
+    connected: false,
   },
   vMix: {
-    connected: false
-  }
+    connected: false,
+  },
 };
 
-let currentScene = 'Multiview Cams';
+let currentScene = "Multiview";
 
 let scenes = {
-  'Quad A': {
-    'Top Left': {
-      type: 'Game',
-      input: 'Team 1 A'
+  "Quad A": {
+    "Top Left": {
+      type: "Game",
+      input: "Team 1 A",
     },
-    'Top Right': {
-      type: 'Game',
-      input: 'Team 1 A'
+    "Top Right": {
+      type: "Game",
+      input: "Team 1 A",
     },
-    'Bottom Left': {
-      type: 'Game',
-      input: 'Team 3 A'
+    "Bottom Left": {
+      type: "Game",
+      input: "Team 1 A",
     },
-    'Bottom Right': {
-      type: 'Game',
-      input: 'Team 1 A'
+    "Bottom Right": {
+      type: "Game",
+      input: "Team 1 A",
     },
   },
-  'Dual with Cams A': {
-    'Top Left': {
-      type: 'Game',
-      input: 'Team 1 A'
+  "Quad B": {
+    "Top Left": {
+      type: "Game",
+      input: "Team 1 A",
     },
-    'Top Right': {
-      type: 'Game',
-      input: 'Team 1 A'
+    "Top Right": {
+      type: "Game",
+      input: "Team 1 A",
     },
-    'Bottom Left': {
-      type: 'Cam',
-      input: 'Team 4 A'
+    "Bottom Left": {
+      type: "Game",
+      input: "Team 1 A",
     },
-    'Bottom Right': {
-      type: 'Cam',
-      input: 'Team 1 A'
+    "Bottom Right": {
+      type: "Game",
+      input: "Team 1 A",
     },
-  }
-}
-
-// let scenes = {
-//   'Quad A': {
-//     'Top Left': 'Team 1 A',
-//     'Top Right': 'Team 1 A',
-//     'Bottom Left': 'Team 1 A',
-//     'Bottom Right': 'Team 1 A'
-//   },
-//   'Quad B': {
-//     'Top Left': 'Team 1 A',
-//     'Top Right': 'Team 1 A',
-//     'Bottom Left': 'Team 1 A',
-//     'Bottom Right': 'Team 1 A'
-//   },
-//   'Dual A': {
-//     'Left': 'Team 1 A',
-//     'Right': 'Team 1 A'
-//   },
-//   'Dual B': {
-//     'Left': 'Team 1 A',
-//     'Right': 'Team 1 A'
-//   },
-//   'Dual with Cams A': {
-//     'Left': 'Team 1 A',
-//     'Right': 'Team 1 A',
-//     'Left Cam': 'Team 1 A',
-//     'Right Cam': 'Team 1 A'
-//   },
-//   'Dual with Cams B': {
-//     'Left': 'Team 1 A',
-//     'Right': 'Team 1 A',
-//     'Left Cam': 'Team 1 A',
-//     'Right Cam': 'Team 1 A'
-//   },
-// }
-
-const expectedScenes = ['Quad A', 'Quad B', 'Dual A', 'Dual B', 'Dual with Cams A', 'Dual with Cams B'];
+  },
+  "Dual A": {
+    "Top Left": {
+      type: "Game",
+      input: "Team 1 A",
+    },
+    "Top Right": {
+      type: "Game",
+      input: "Team 1 A",
+    },
+  },
+  "Dual B": {
+    "Top Left": {
+      type: "Game",
+      input: "Team 1 A",
+    },
+    "Top Right": {
+      type: "Game",
+      input: "Team 1 A",
+    },
+  },
+  "Dual with Cams A": {
+    "Top Left": {
+      type: "Game",
+      input: "Team 1 A",
+    },
+    "Top Right": {
+      type: "Game",
+      input: "Team 1 A",
+    },
+    "Left Cam": {
+      type: "Cam",
+      input: "Team 1 A",
+    },
+    "Right Cam": {
+      type: "Cam",
+      input: "Team 1 A",
+    },
+  },
+  "Dual with Cams B": {
+    "Top Left": {
+      type: "Game",
+      input: "Team 1 A",
+    },
+    "Top Right": {
+      type: "Game",
+      input: "Team 1 A",
+    },
+    "Left Cam": {
+      type: "Cam",
+      input: "Team 1 A",
+    },
+    "Right Cam": {
+      type: "Cam",
+      input: "Team 1 A",
+    },
+  },
+};
 
 const httpServer = createServer();
 
 const init = async () => {
-  const db = new DB(system);
-
   const io = new Server(httpServer, {
     cors: {
       origin: `http://localhost:3000`,
-      methods: ["GET", "POST"]
-    }
+      methods: ["GET", "POST"],
+    },
   });
 
   const obs1 = new OBS(system, io, {
-    address: 'localhost',
-    port: '4444',
-    password: '',
-    name: 'OBS1'
-  })
+    address: process.env.OBS1ADDRESS,
+    port: process.env.OBS1PORT,
+    password: "",
+    name: "OBS1",
+  });
 
   try {
     await obs1.connect();
-    state['OBS1'].connected = true;
+    state["OBS1"].connected = true;
   } catch (err) {
     console.log(err);
   }
 
-  if (useOBS3) {
-    const obs3 = new OBS(system, io, {
-      address: 'localhost',
-      port: '4444',
-      password: '',
-      name: 'OBS3'
-    })
+  let obs2;
+
+  if (useOBS2) {
+    obs2 = new OBS(system, io, {
+      address: process.env.OBS2ADDRESS,
+      port: process.env.OBS2PORT,
+      password: "",
+      name: "OBS2",
+    });
 
     try {
-      await obs3.connect();
-      state['OBS3'].connected = true;
+      await obs2.connect();
+      state["OBS2"].connected = true;
     } catch (err) {
       console.log(err);
     }
   }
 
-  await setInitialSceneItemValues(obs1);
+  let obs3;
 
-  // DB example
-  // system.emit('db_get', 'userIPs', userIPs => {
-  //   if (userIPs === undefined) {
-  //     store.userIPs = {
-  //       1: '192.168.1.1:8888',
-  //       2: '192.168.1.2:8888',
-  //       3: '192.168.1.3:8888',
-  //       4: '192.168.1.4:8888',
-  //     };
+  if (useOBS3) {
+    obs3 = new OBS(system, io, {
+      address: process.env.OBS3ADDRESS,
+      port: process.env.OBS3PORT,
+      password: "",
+      name: "OBS3",
+    });
 
-  //     system.emit('db_set', 'userIPs', store.userIPs)
-  //   } else {
-  //     store.userIPs = userIPs;
-  //   }
-  // })
+    try {
+      await obs3.connect();
+      state["OBS3"].connected = true;
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
+  await setInitialSceneItemValues(obs1, obs2, obs3);
 
+  console.log(
+    colors.green.bold(`Websocket available on port: ${process.env.PORT}`)
+  );
 
-  console.log(colors.green.bold(`Websocket available on port: ${process.env.PORT}`));
+  io.on("connect", (socket) => {
+    console.log(colors.blue("New Websocket Connection from:", socket.id));
 
-  io.on('connect', socket => {
-    console.log(colors.blue('New Websocket Connection from:', socket.id));
-
-    socket.on('disconnect', reason => {
+    socket.on("disconnect", (reason) => {
       console.log(colors.red(`${socket.id} websocket disconnected: ${reason}`));
     });
 
-    socket.emit('init', state);
+    socket.emit("init", state);
 
-    socket.emit('sceneInfo', scenes);
+    socket.emit("sceneInfo", scenes);
 
-    socket.emit('currentScene', currentScene);
+    socket.emit("currentScene", currentScene);
 
-    socket.on('handleChange', (scene, position, input) => {
+    socket.on("handleChange", (scene, position, input) => {
       if (scene === currentScene) return;
 
-      console.log('CHANGE', scene, position, input);
+      let type = "Game";
 
-      scenes[scene][position] = input;
+      if (position.includes("Cam")) {
+        type = "Cam";
+      }
 
-      handleChange(scene, position, input, obs1);
+      scenes[scene][position]["input"] = input;
+      scenes[scene][position]["type"] = type;
 
-      io.emit('sceneInfo', scenes);
+      handleChange(scene, position, input, type, obs1, obs2, obs3);
+
+      io.emit("sceneInfo", scenes);
     });
 
-    socket.on('handleTransition', scene => {
-      console.log('TRANSITION', scene);
+    socket.on("handleTransition", (scene) => {
+      obs1.setCurrentScene(scene);
+      if (useOBS2) obs2.setCurrentScene(scene);
+      if (useOBS3) obs3.setCurrentScene(scene);
 
       currentScene = scene;
-      io.emit('currentScene', currentScene);
-    })
-
+      io.emit("currentScene", currentScene);
+    });
   });
 
   httpServer.listen(process.env.PORT);
-
-}
+};
 
 init();
 
-const findOBS = input => {
-  if (input === 'Team 1 A' || input === 'Team 1 B' || input === 'Team 2 A' || input === 'Team 2 B') {
-    return 'OBS1';
-  } else if (input === 'Team 3 A' || input === 'Team 3 B' || input === 'Team 4 A' || input === 'Team 4 B') {
-    return 'OBS2';
-  } else if (input === 'Team 5 A' || input === 'Team 5 B' || input === 'Team 6 A' || input === 'Team 6 B') {
-    return 'OBS3';
+const findOBS = (input) => {
+  if (
+    input === "Team 1 A" ||
+    input === "Team 1 B" ||
+    input === "Team 2 A" ||
+    input === "Team 2 B"
+  ) {
+    return "OBS1";
+  } else if (
+    input === "Team 3 A" ||
+    input === "Team 3 B" ||
+    input === "Team 4 A" ||
+    input === "Team 4 B"
+  ) {
+    return "OBS2";
+  } else if (
+    input === "Team 5 A" ||
+    input === "Team 5 B" ||
+    input === "Team 6 A" ||
+    input === "Team 6 B"
+  ) {
+    return "OBS3";
   } else {
     return null;
   }
-}
+};
 
-const getPositionValues = position => {
+const getPositionValues = (position) => {
   switch (position) {
-    case 'Top Left':
+    case "Top Left":
       return { x: 0, y: 0, scale: 0.5 };
-    case 'Top Right':
+    case "Top Right":
       return { x: 960, y: 0, scale: 0.5 };
-    case 'Bottom Left':
+    case "Bottom Left":
       return { x: 0, y: 540, scale: 0.5 };
-    case 'Bottom Right':
+    case "Bottom Right":
       return { x: 960, y: 540, scale: 0.5 };
-    case 'Left':
+    case "Left":
       return { x: 0, y: 0, scale: 0.5 };
-    case 'Right':
+    case "Right":
       return { x: 960, y: 0, scale: 0.5 };
-    case 'Left Cam':
+    case "Left Cam":
       return { x: 0, y: 540, scale: 0.5 };
-    case 'Right Cam':
+    case "Right Cam":
       return { x: 960, y: 540, scale: 0.5 };
     default:
       return;
   }
-}
+};
 
-const getPositionLabel = (xposition, yposition, scale) => {
-  switch (true) {
-    case (xposition === 0 && yposition === 0 && scale === 0.5):
-      return 'Top Left';
-    case (xposition === 960 && yposition === 0 && scale === 0.5):
-      return 'Top Right';
-    case (xposition === 0 && yposition === 540 && scale === 0.5):
-      return 'Bottom Left';
-    case (xposition === 960 && yposition === 540 && scale === 0.5):
-      return 'Bottom Right';
-    default:
-      return;
-  }
-}
-
-const deleteAllItemsInScene = async (obs, sceneName) => {
-  const sceneItems = await obs.getSceneItemList(sceneName);
-  for (const sceneItem of sceneItems) {
-    await obs.deleteSceneItem(sceneName, sceneItem.itemId);
-  }
-}
-
-const addItemsToScene = async (obs1, sceneName) => {
-  const scenePositions = Object.keys(scenes[sceneName]);
-  for (const scenePosition of scenePositions) {
-    const { type, input } = scenes[sceneName][scenePosition];
-
-    const obs = findOBS(input);
-
-    const position = getPositionValues(scenePosition);
-
-    switch (obs) {
-      case 'OBS1':
-        const id = await obs1.addSceneItem(sceneName, `${type} ${input}`);
-
-        await obs1.setSceneItemProperties(sceneName, id, position.x, position.y, position.scale)
-          .then()
-          .catch(err => {
-            console.log(err);
-          })
+const getPositionLabel = (xposition, yposition, scale, type) => {
+  if (type === "Game") {
+    switch (true) {
+      case xposition === 0 && yposition === 0 && scale === 0.5:
+        return "Top Left";
+      case xposition === 960 && yposition === 0 && scale === 0.5:
+        return "Top Right";
+      case xposition === 0 && yposition === 540 && scale === 0.5:
+        return "Bottom Left";
+      case xposition === 960 && yposition === 540 && scale === 0.5:
+        return "Bottom Right";
       default:
-
+        return;
+    }
+  } else {
+    switch (true) {
+      case xposition === 0 && yposition === 540 && scale === 0.5:
+        return "Left Cam";
+      case xposition === 960 && yposition === 540 && scale === 0.5:
+        return "Right Cam";
+      default:
+        return;
     }
   }
-}
+};
 
-const setInitialSceneItemValues = async (obs1) => {
+const deleteAllItemsInScene = async (obs, scene) => {
+  const sceneItems = await obs.getSceneItemList(scene);
+  for (const { itemId } of sceneItems) {
+    await obs.deleteSceneItem(scene, itemId);
+  }
+};
+
+const addItemsToScene = async (obs1, obs2, obs3, scene) => {
+  const positions = Object.keys(scenes[scene]);
+  for (const position of positions) {
+    const { type, input } = scenes[scene][position];
+
+    selectOBSAndAddItem(obs1, obs2, obs3, input, position, scene, type);
+  }
+};
+
+const selectOBSAndAddItem = (
+  obs1,
+  obs2,
+  obs3,
+  input,
+  position,
+  scene,
+  type
+) => {
+  const obs = findOBS(input);
+
+  const positionValues = getPositionValues(position);
+
+  switch (obs) {
+    case "OBS1":
+      addItemToScene(obs1, scene, type, input, positionValues);
+      break;
+    case "OBS2":
+      addItemToScene(obs2, scene, type, input, positionValues);
+      break;
+    case "OBS3":
+      addItemToScene(obs3, scene, type, input, positionValues);
+      break;
+    default:
+      return;
+  }
+};
+
+const addItemToScene = async (obs, sceneName, type, input, position) => {
+  const id = await obs.addSceneItem(sceneName, `${type} ${input}`);
+
+  await obs
+    .setSceneItemProperties(
+      sceneName,
+      id,
+      position.x,
+      position.y,
+      position.scale
+    )
+    .then()
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const setInitialSceneItemValues = async (obs1, obs2, obs3) => {
   const sceneNames = Object.keys(scenes);
 
-  for (const sceneName of sceneNames) {
-    await deleteAllItemsInScene(obs1, sceneName);
-    await addItemsToScene(obs1, sceneName);
+  for (const scene of sceneNames) {
+    await deleteAllItemsInScene(obs1, scene);
+
+    if (useOBS2) {
+      await deleteAllItemsInScene(obs2, scene);
+    }
+
+    if (useOBS3) {
+      await deleteAllItemsInScene(obs3, scene);
+    }
+
+    await addItemsToScene(obs1, obs2, obs3, scene);
   }
-}
+};
 
+const deleteItemInPosition = async (scene, position, type, obs) => {
+  const sceneItems = await obs.getSceneItemList(scene);
 
-const handleChange = async (scene, position, input, obs1) => {
-  // get items from scene
+  for (const { itemId } of sceneItems) {
+    const itemProperties = await obs.getSceneItemProperties(scene, itemId);
 
-  // find the one in the maching position
-
-  // if it exists - delete it
-
-  const sceneItems = await obs1.getSceneItemList(scene);
-
-  for (const sceneItem of sceneItems) {
-
-    console.log(sceneItem);
-
-    const itemProperties = await obs1.getSceneItemProperties(scene, sceneItem.itemId);
-
-    // console.log('ITEM PROPERTIES', itemProperties);
-
-    const positionLabel = getPositionLabel(itemProperties.position.x, itemProperties.position.y, itemProperties.scale.x);
-
-    console.log('POSITION LABEL', positionLabel);
+    const positionLabel = getPositionLabel(
+      itemProperties.position.x,
+      itemProperties.position.y,
+      itemProperties.scale.x,
+      type
+    );
 
     if (positionLabel === position) {
-      console.log('delete this item', sceneItem.itemId);
+      await obs.deleteSceneItem(scene, itemId);
     }
   }
-}
+};
+
+const handleChange = async (scene, position, input, type, obs1, obs2, obs3) => {
+  deleteItemInPosition(scene, position, type, obs1);
+  if (useOBS2) deleteItemInPosition(scene, position, type, obs2);
+  if (useOBS3) deleteItemInPosition(scene, position, type, obs3);
+
+  selectOBSAndAddItem(obs1, obs2, obs3, input, position, scene, type);
+};
